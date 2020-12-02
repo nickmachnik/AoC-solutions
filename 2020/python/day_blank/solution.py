a3c71bc309340e02f33447ff8cc2574f7af7ed72
Nick Machnik <nick.machnik@gmail.com>
2020-12-02 12:46:06 +0100

    Augment blank with prints and tests

--- 2020/python/day_blank/solution.py
+++ 2020/python/day_blank/solution.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
 def main():
-    part_one()
-    part_two()
+    data = load_data("puzzleinput.txt")
+    print("Answer part one:", part_one(data))
+    print("Answer part two:", part_two(data))
 
 
-def part_one():
+def part_one(data):
     pass
 
 
-def part_two():
+def part_two(data):
     pass
+
+
+def load_data(path):
+    input_numbers = []
+    with open(path, 'r') as fin:
+        for line in fin:
+            input_numbers.append(int(line.strip()))
+    return input_numbers
 
 
 if __name__ == '__main__':
---
+++ 2020/python/day_blank/test_solution.py
@@ -0,0 +1,17 @@
+from solution import part_one, part_two, load_data
+
+
+def test_loading():
+    test_numbers = load_data("testdata.txt")
+    pass
+    # assert test_numbers == [1721, 979, 366, 299, 675, 1456]
+
+
+def test_part_one():
+    pass
+    # assert part_one(load_data("testdata.txt")) == 514579
+
+
+def test_part_two():
+    pass
+    # assert part_two(load_data("testdata.txt")) == 241861950
