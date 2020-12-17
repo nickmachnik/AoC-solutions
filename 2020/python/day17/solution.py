58fb62d56281079366ba47f620a31870d9d64907
Nick Machnik <nick.machnik@gmail.com>
2020-12-17 11:29:49 +0100

    Make dimensionality a class attribute

--- 2020/python/day17/solution.py
+++ 2020/python/day17/solution.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+from itertools import product
+
+
 class PocketDimension:
-    def __init__(self, path):
+    def __init__(self, path, dimensionality):
+        self.ndim = dimensionality
         self.load_initial_state(path)
 
     def load_initial_state(self, path):
@@ -11,50 +15,41 @@ class PocketDimension:
                 line = line.strip()
                 for x, symbol in enumerate(line):
                     if symbol == '#':
-                        self.active_cubes.add((x, y, 0, 0))
+                        self.active_cubes.add(
+                            tuple([x, y] + [0] * (self.ndim - 2)))
 
-    def cycle(self, neighbor_function):
+    def cycle(self):
         inactive_candidates = set()
         active_in_next_cycle = set()
 
-        for x, y, z, w in self.active_cubes:
+        for cube in self.active_cubes:
             active_neighbors = 0
-            for neighbor in neighbor_function(x, y, z, w):
-                if neighbor in self.active_cubes:
+            for neighbor in self.neighbors(cube):
+                if neighbor == cube:
+                    continue
+                elif neighbor in self.active_cubes:
                     active_neighbors += 1
                 else:
                     inactive_candidates.add(neighbor)
             if active_neighbors == 2 or active_neighbors == 3:
-                active_in_next_cycle.add((x, y, z, w))
+                active_in_next_cycle.add(cube)
 
-        for x, y, z, w in inactive_candidates:
+        for cube in inactive_candidates:
             active_neighbors = 0
-            for neighbor in neighbor_function(x, y, z, w):
+            for neighbor in self.neighbors(cube):
                 if neighbor in self.active_cubes:
                     active_neighbors += 1
             if active_neighbors == 3:
-                active_in_next_cycle.add((x, y, z, w))
+                active_in_next_cycle.add(cube)
 
         self.active_cubes = active_in_next_cycle
 
-    @staticmethod
-    def neighbors3D(x, y, z, w):
-        for nx in [x - 1, x, x + 1]:
-            for ny in [y - 1, y, y + 1]:
-                for nz in [z - 1, z, z + 1]:
-                    if nx == x and ny == y and nz == z:
-                        continue
-                    yield nx, ny, nz, w
-
-    @staticmethod
-    def neighbors4D(x, y, z, w):
-        for nx in [x - 1, x, x + 1]:
-            for ny in [y - 1, y, y + 1]:
-                for nz in [z - 1, z, z + 1]:
-                    for nw in [w - 1, w, w + 1]:
-                        if nx == x and ny == y and nz == z and nw == w:
-                            continue
-                        yield nx, ny, nz, nw
+    def neighbors(self, cube):
+        return product(
+            *[
+                [cube[i] - 1, cube[i], cube[i] + 1]
+                for i in range(self.ndim)]
+            )
 
     def num_active_cubes(self):
         return len(self.active_cubes)
@@ -66,16 +61,16 @@ def main():
 
 
 def part_one(path):
-    pd = PocketDimension(path)
+    pd = PocketDimension(path, 3)
     for i in range(6):
-        pd.cycle(neighbor_function=PocketDimension.neighbors3D)
+        pd.cycle()
     return pd.num_active_cubes()
 
 
 def part_two(path):
-    pd = PocketDimension(path)
+    pd = PocketDimension(path, 4)
     for i in range(6):
-        pd.cycle(neighbor_function=PocketDimension.neighbors4D)
+        pd.cycle()
     return pd.num_active_cubes()
 
 
