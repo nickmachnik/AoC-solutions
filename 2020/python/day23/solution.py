10627e2a37e486fceec6be0003c41efe334cfb09
Nick Machnik <nick.machnik@gmail.com>
2020-12-24 13:58:49 +0100

    Day23 solved

--- 2020/python/day23/solution.py
+++ 2020/python/day23/solution.py
@@ -2,9 +2,12 @@
 
 class CircularArray:
     class Node:
-        def __init__(self):
+        def __init__(self, arr):
+            self.arr = arr
             self.nxt = None
+            self.prv = None
             self.val = None
+            self.dst = None
 
         def set_val(self, val):
             self.val = val
@@ -12,20 +15,54 @@ class CircularArray:
         def set_nxt(self, nxt):
             self.nxt = nxt
 
+        def set_prv(self, prv):
+            self.prv = prv
+
+        def set_dst(self, dst):
+            self.dst = dst
+
+        def find_dest(self):
+            dest_val = self.arr.max if self.val == 1 else self.val - 1
+            cand_down = self.prv
+            cand_up = self.nxt
+            while cand_down.val != dest_val and cand_up.val != dest_val:
+                cand_down = cand_down.prv
+                cand_up = cand_up.nxt
+            self.dst = cand_down if cand_down.val == dest_val else cand_up
+
     def __init__(self, numstr):
+        self.curr_cup = None
+        self.cycled = 0
+        self.max = 0
         prev = None
         for num in numstr:
             prev = self.insert_after(prev, int(num))
 
+    def find_dests(self):
+        curr_cup = self.curr_cup
+        curr_cup.find_dest()
+        curr_cup = curr_cup.nxt
+        while curr_cup != self.curr_cup:
+            curr_cup.find_dest()
+            curr_cup = curr_cup.nxt
+
     def insert_after(self, prev, val):
-        new = CircularArray.Node()
+        new = CircularArray.Node(self)
         new.set_val(val)
         if prev is None:
             new.set_nxt(new)
+            new.set_prv(new)
             self.curr_cup = new
+            self.prev_dest = new
         else:
             new.set_nxt(prev.nxt)
+            new.nxt.set_prv(new)
+            new.set_prv(prev)
             prev.set_nxt(new)
+        if val == 1:
+            self.one = new
+        if self.max < val:
+            self.max = val
         return new
 
     def __repr__(self):
@@ -36,6 +73,31 @@ class CircularArray:
             curr_cup = curr_cup.nxt
         return "".join(res)
 
+    def cycle(self):
+        pu_start = self.curr_cup.nxt
+        pu_mid = pu_start.nxt
+        pu_end = pu_mid.nxt
+        pu = set([pu_start, pu_mid, pu_end])
+        dest = self.curr_cup.dst
+        while dest in pu:
+            for node in pu:
+                if node is dest:
+                    dest = node.dst
+
+        self.curr_cup.set_nxt(pu_end.nxt)
+        pu_end.nxt.set_prv(self.curr_cup)
+
+        pu_end.set_nxt(dest.nxt)
+        dest.nxt.set_prv(pu_end)
+
+        dest.set_nxt(pu_start)
+        pu_start.set_prv(dest)
+
+        self.prev_dest = dest
+        self.curr_cup = self.curr_cup.nxt
+
+        self.cycled += 1
+
 
 def main():
     mystr = "523764819"
@@ -45,24 +107,10 @@ def main():
 
 def part_one(numstr):
     carr = CircularArray(numstr)
+    carr.find_dests()
+
     for i in range(100):
-        dest_val = 9 if carr.curr_cup.val == 1 else carr.curr_cup.val - 1
-        pu_start = carr.curr_cup.nxt
-        pu_mid = pu_start.nxt
-        pu_end = pu_mid.nxt
-        pu_vals = set([pu_start.val, pu_mid.val, pu_end.val])
-        while dest_val in pu_vals:
-            dest_val -= 1
-            if dest_val == 0:
-                dest_val = 9
-        # find destination cup
-        dest_cup = pu_end.nxt
-        while dest_cup.val != dest_val:
-            dest_cup = dest_cup.nxt
-        carr.curr_cup.nxt = pu_end.nxt
-        pu_end.nxt = dest_cup.nxt
-        dest_cup.nxt = pu_start
-        carr.curr_cup = carr.curr_cup.nxt
+        carr.cycle()
 
     # find cup one
     curr_cup = carr.curr_cup
@@ -78,8 +126,20 @@ def part_one(numstr):
     return "".join(order)
 
 
-def part_two(data):
-    pass
+def part_two(numstr):
+    max_num = 10 ** 6
+    carr = CircularArray(numstr)
+    prev = carr.curr_cup
+    for i in range(8):
+        prev = prev.nxt
+    for num in range(10, max_num + 1):
+        prev = carr.insert_after(prev, num)
+    carr.find_dests()
+
+    for i in range(max_num * 10):
+        carr.cycle()
+
+    return carr.one.nxt.val * carr.one.nxt.nxt.val
 
 
 if __name__ == '__main__':
--- 2020/python/day23/test_solution.py
+++ 2020/python/day23/test_solution.py
@@ -3,7 +3,8 @@ from solution import part_one, part_two
 
 def test_part_one():
     assert part_one("389125467") == "67384529"
+    389125467
 
 
 def test_part_two():
-    assert part_two("data") == 291
+    assert part_two("389125467") == 149245887792
